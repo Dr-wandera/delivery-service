587a37bb4cfcde86d132a9709fd4947eed64bad9
Dr-wandera <wanderafaustine@gmail.com>
2026-05-14 16:53:33 +0300

    initial commit

---
+++ src/test/java/com/wanderaTech/delivery_service/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderaTech.delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
