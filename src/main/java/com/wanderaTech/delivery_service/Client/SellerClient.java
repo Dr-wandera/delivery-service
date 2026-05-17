81c60ecc8c8ac499f7853e8c21d9589901590268
Dr-wandera <wanderafaustine@gmail.com>
2026-05-17 17:11:04 +0300

    commented

--- src/main/java/com/wanderaTech/delivery_service/Client/SellerClient.java
+++ src/main/java/com/wanderaTech/delivery_service/Client/SellerClient.java
@@ -3,13 +3,13 @@ package com.wanderaTech.delivery_service.Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class SellerClient {
-
-    private final WebClient.Builder webClientBuilder;
-
-    public SellerClient(){
-        
-    }
-}
+//@Service
+//@RequiredArgsConstructor
+//public class SellerClient {
+//
+////    private final WebClient.Builder webClientBuilder;
+//
+//    public SellerClient(){
+//
+//    }
+//}
